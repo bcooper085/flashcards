17ca4ca4e01b3297c5ad814d938c7232da6efe70
Brandon Cooper and Cassie Musolf <cassie@>
2017-01-10 11:59:17 -0800

    add background color, add heading underline and cursor, hide paragraph text

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,55 @@
 $(function() {
-  $("h3").click(function () {
-    $("p").show();
+  $("#javascript").click(function () {
+    $("#javascript").hide();
+    $(".java").slideToggle();
+  });
+
+  $(".java").click(function () {
+    $(".java").hide();
+    $("#javascript").fadeIn();
+  })
+
+  $("#operators").click(function () {
+    $(".operators").slideToggle();
+  });
+
+  $("#variables").click(function () {
+    $(".variables").slideToggle();
+  });
+
+  $("#methods").click(function () {
+    $(".methods").slideToggle();
+  });
+
+  $("#arguments").click(function () {
+    $(".arguments").slideToggle();
+  });
+
+  $("#parameters").click(function () {
+    $(".parameters").slideToggle();
+  });
+
+  $("#booleans").click(function () {
+    $(".booleans").slideToggle();
+  });
+
+  $("#functions").click(function () {
+    $(".functions").slideToggle();
+  });
+
+  $("#undefined").click(function () {
+    $(".undefined").slideToggle();
+  });
+
+  $("#strings").click(function () {
+    $(".strings").slideToggle();
+  });
+
+  $("#nan").click(function () {
+    $(".nan").slideToggle();
+  });
+
+  $("#jquery").click(function () {
+    $(".jquery").slideToggle();
   });
 });
